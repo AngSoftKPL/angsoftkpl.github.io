db0a7a8a9cfa540601789995c3c6f706a621bd08
AngSoftKPL <adolfo_a1513@hotmail.com>
2020-05-05 00:42:44 -0500

    Se agrega encendido

--- Birthday/scripts/index.js
+++ Birthday/scripts/index.js
@@ -2,3 +2,10 @@ $(window).load(function(){
 	$('.loading').fadeOut('fast');
 	$('.container').fadeIn('fast');
 });
+
+$('document').ready(function(){
+    $('#turn_on').click(function(){
+        $('body').addClass('peach');
+		$(this).fadeOut('slow').delay(5000);
+    });
+});
